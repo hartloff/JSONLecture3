8ca30ddf04e8d4d8cfbaed1980e19fc3778e274d
jessehartloff <hartloff@buffalo.edu>
2017-04-13 17:20:35 -0400

    initial commit

---
+++ src/JSONLecture3.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class JSONLecture3 {
+
+
+    // 2 points
+    static ArrayList<String> Q1(String inputJson){
+        // given a JSON String in the format of an array of objects where each object is in the format
+        // {"name":"String","number":147}
+        // return the value of "name" for each object in an ArrayList
+
+
+        return null;
+    }
+
+
+    // 3 points
+    static String Q2(String inputJson){
+        // given a JSON String in the format of an array of objects where each object is in the format
+        // {"name":"String","number":147}
+        // return the value of "name" of the object with the largest value for "number"
+
+
+        return "";
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
